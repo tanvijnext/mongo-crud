c2e9916b4ffdbf9a2e2e29bceed40c06bb7b5e96
tanvi <tanvi.jnext@gmail.com>
2022-04-14 16:58:57 +0530

    Adding script for heroku

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
-require('./config/db.js')
+    // require('./config/db.js')
 const express = require('express')
 const bodyParser = require('body-parser')
-const api = require('./routes/userApi')
+    // const api = require('./routes/userApi')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
+app.get('/', (req, res) => {
+    res.send("text")
+})
 app.use(bodyParser.json())
 
 const PORT = process.env.PORT
@@ -17,4 +19,4 @@ app.listen(PORT, () => {
     console.log(`app running on port ${PORT}`)
 })
 
-app.use('/api', api)
+// app.use('/api', api)
